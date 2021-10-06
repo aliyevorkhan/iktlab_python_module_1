a27625d19523a2f765c2cb0b4163e42c72fb67f4
Orkhan ALIYEV <338396@ogr.ktu.edu.tr>
2021-10-06 20:04:45 +0400

    Create ders_1_01102021.py

---
+++ ders_1_01102021.py
@@ -0,0 +1,23 @@
+isc_maas_fev = 1500 #integer
+print(isc_maas_fev, type(isc_maas_fev))
+isc_maas_fev = "python"
+print(isc_maas_fev, type(isc_maas_fev))
+isc_maas_fev = 1500.15 #float
+print(isc_maas_fev, type(isc_maas_fev))
+
+#strings
+str = 'A'
+email= "oooso@ksks.com"
+conflict = 'Orkhan"s class'
+
+print(str, email, conflict, end='\n')
+print(type(str), type(email), type(conflict))
+print(' ')
+print("salam dunya\n")
+print("salam\ndunya")
+
+'''
+bir nece setirden ibaret
+comment
+setiri
+'''
