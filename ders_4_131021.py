43e786fe9928ce0bfd8e0ac7e581c5faa772bd15
Orkhan ALIYEV <338396@ogr.ktu.edu.tr>
2021-10-15 19:00:01 +0400

    Create ders_4_131021.py

---
+++ ders_4_131021.py
@@ -0,0 +1,54 @@
+i = 1 
+# if i < 6:
+#     print(i)
+#     i=i+1
+# if i<6:
+#     print(i)
+#     i=i+1
+# if i<6:
+#     print(i)
+#     i=i+1
+# if i<6:
+#     print(i)
+#     i=i+1
+# if i<6:
+#     print(i)
+#     i=i+1
+    
+#esl hell yolu
+# while i<=6:
+#     print(i)
+#     i=i+1
+
+#*************** break istifadesi
+# i = 1
+# while i < 6:
+#     print(i)
+#     if i == 3:
+#         break
+#     i += 1
+
+# print("dongunu break ile qirib cixdi")
+
+#************ continue istifadesi
+# i = 0
+# while i < 6:
+#   i += 1
+#   if i == 3:
+#     continue
+#   print(i)
+
+#************ while/else
+# i = 1
+# while i < 6:
+#   print(i)
+#   i += 1
+# else:
+#   print("i is no longer less than 6")
+#******************* sonsuz dongu 
+# i = 1
+# while i<6:
+#     print(i)
+#*************** sonsuz dongu 2
+while True:
+    print("HELLO")
